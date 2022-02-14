b476f335ca4e3c8fb3e2fba5b5df1bbe96d8bdb1
andrei1112111 <74547328+andrei1112111@users.noreply.github.com>
2022-02-14 23:16:30 +0700

    Большая задача по Maps API. Часть №9

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import requests
 
 
-def geo_search(search):
+def geo_search(search, ch=False):
     geocoder_request = \
         f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b" \
         f"&geocode={search}&format=json"
@@ -12,10 +12,18 @@ def geo_search(search):
         json_response = response.json()
         toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
         toponym = toponym["Point"]["pos"]
+        if ch:
+            ch = ''
+        else:
+            try:
+                ch = json_response['response']['GeoObjectCollection']['featureMember'][0][
+                    'GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['postal_code'] + '; '
+            except Exception:
+                ch = ('(Нет почтового кода); ')
         print(json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'])
         return toponym, \
-               json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty'][
-                   'GeocoderMetaData']['text']
+               ch + json_response['response']['GeoObjectCollection']['featureMember'][0][
+                   'GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
     else:
         print("Ошибка выполнения запроса:")
         print("Http статус:", response.status_code, "(", response.reason, ")")
@@ -56,11 +64,15 @@ def main():
     global scale, x, y, mapp, address, pt
     pygame.init()
     screen = pygame.display.set_mode((600, 610))
-    pygame.display.set_caption('Большая задача по Maps API. Часть №8')
+    pygame.display.set_caption('Большая задача по Maps API. Часть №9')
     clock = pygame.time.Clock()
     font = pygame.font.Font(None, 32)
+    font2 = pygame.font.Font(None, 18)
     input_box = pygame.Rect(10, 460, 300, 40)
     obj_addr = pygame.Rect(10, 510, 580, 40)
+    pr1 = pygame.Rect(360, 560, 40, 40)
+    pr2 = pygame.Rect(400, 560, 40, 40)
+    pr_ch = False
     obj_addr_text = ''
     reset_button = pygame.Rect(10, 560, 335, 40)
     active = False
@@ -130,11 +142,13 @@ def main():
                     pt = None
                     ch = True
                     obj_addr_text = ''
+                if pr1.collidepoint(event.pos) or pr2.collidepoint(event.pos):
+                    pr_ch = not pr_ch
             if event.type == pygame.KEYDOWN:
                 if active:
                     if event.key == pygame.K_RETURN:
                         address = text.strip().replace(' ', ', ')
-                        pt, obj_addr_text = geo_search(address)
+                        pt, obj_addr_text = geo_search(address, pr_ch)
                         x = y = 0
                         ch = True
                     elif event.key == pygame.K_BACKSPACE:
@@ -155,13 +169,20 @@ def main():
             c = pygame.color.Color('black')
         pygame.draw.rect(screen, c, input_box, 2)
 
-        screen.blit(font.render(obj_addr_text, True, pygame.color.Color('black')),
+        screen.blit(font2.render(obj_addr_text, True, pygame.color.Color('black')),
                     (obj_addr.x + 5, obj_addr.y + 5))
         pygame.draw.rect(screen, pygame.color.Color('black'), obj_addr, 2)
 
         screen.blit(font.render('Сброс поискового результата', True, pygame.color.Color('black')),
                     (reset_button.x + 5, reset_button.y + 5))
         pygame.draw.rect(screen, pygame.color.Color('black'), reset_button, 2)
+
+        if not pr_ch:
+            pygame.draw.rect(screen, pygame.color.Color('black'), pr1)
+            pygame.draw.rect(screen, pygame.color.Color('black'), pr2, 2)
+        else:
+            pygame.draw.rect(screen, pygame.color.Color('black'), pr1, 2)
+            pygame.draw.rect(screen, pygame.color.Color('black'), pr2)
 
         pygame.display.flip()
 
